52ed520d69eee691a3d82940eb8ed069d3360cc5
niaz <nahmed1471@gmail.com>
2025-03-09 18:00:55 +0800

    complete contructor

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, height, width):
-        self.height = height
-        self.width = width
+        self.__root = Tk()
+        self.__root.title("Maze Solver") 
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack()
+        self.is_running = False
